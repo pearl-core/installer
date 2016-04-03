35a2244c2a858bc92c32a8886cbeb016a508e619
Filippo Squillace <squillace.filippo@gmail.com>
2016-04-03 14:17:25 +0100

    Call directly pearl init to initialize the PEARL_HOME

--- install.sh
+++ install.sh
@@ -11,27 +11,4 @@ PEARL_HOME=$HOME/.config/pearl
 echo "* Downloading Pearl to $PEARL_ROOT"
 git clone --quiet 'https://github.com/pearl-core/pearl.git' $PEARL_ROOT
 
-source $PEARL_ROOT/lib/utils.sh
-
-bold_white; echo -n "* "; normal; echo "Creating Pearl configuration in $PEARL_HOME"
-mkdir -p $PEARL_HOME/repos
-mkdir -p $PEARL_HOME/packages
-
-[ -e "$PEARL_HOME/pearl.conf" ] || cp "$PEARL_ROOT/etc/pearl.conf.template" "$PEARL_HOME/pearl.conf"
-
-apply "source ${PEARL_ROOT}/boot/pearl.sh" ${HOME}/.bashrc
-apply "export PEARL_ROOT=${PEARL_ROOT}" ${HOME}/.bashrc
-bold_white; echo -n "* "; normal; echo "Activated Pearl for Bash"
-apply "source ${PEARL_ROOT}/boot/pearl.sh" ${HOME}/.zshrc
-apply "export PEARL_ROOT=${PEARL_ROOT}" ${HOME}/.zshrc
-bold_white; echo -n "* "; normal; echo "Activated Pearl for Zsh"
-apply "source ${PEARL_ROOT}/boot/pearl.fish" ${HOME}/.config/fish/config.fish
-apply "set -x PEARL_ROOT ${PEARL_ROOT}" ${HOME}/.config/fish/config.fish
-bold_white; echo -n "* "; normal; echo "Activated Pearl for Fish shell"
-apply "source ${PEARL_ROOT}/boot/vim/pearl.vim" ${HOME}/.vimrc
-bold_white; echo -n "* "; normal; echo "Activated Pearl for Vim editor"
-info ""
-info "Done! Open a new terminal and have fun!"
-info ""
-info "To get the list of Pearl packages available:"
-echo "    >> pearl list"
+$PEARL_ROOT/bin/pearl init
