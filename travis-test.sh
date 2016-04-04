d548854da71394413b9f82fa98adb3ffa89deb94
Filippo Squillace <squillace.filippo@gmail.com>
2016-04-04 20:46:48 +0100

    Add .travis.yml

---
+++ travis-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+./install.sh
+
+test -d "$HOME/.config/pearl"
+test -d "$HOME/.local/share/pearl"
+
+source $HOME/.bashrc
+
+echo hello
+test -d "$PEARL_ROOT"
+test -d "$PEARL_HOME"
+test -d "$PEARL_TEMPORARY"
