b14ea4831a749b98330a6ff0125f00c2f52ed5fe
Filippo Squillace <squillace.filippo@gmail.com>
2017-06-26 16:14:11 +0200

    Add test-utils as dependency

--- tests/integ-tests/install-osx-deps.sh
+++ tests/integ-tests/install-osx-deps.sh
@@ -7,6 +7,6 @@ brew update
 # Openssl is required for installing git from source code
 brew install openssl
 
-./tests/integ-tests/install-bash.sh "$TRAVIS_BASH_VERSION"
-./tests/integ-tests/install-git.sh "$TRAVIS_GIT_VERSION"
+./tests/test-utils/install-bash.sh "$TRAVIS_BASH_VERSION"
+./tests/test-utils/install-git.sh "$TRAVIS_GIT_VERSION"
 
