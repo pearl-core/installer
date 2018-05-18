7d22c88f50068b7ca124378b3c88a03b3fdf58c4
go2null <1t1is2@gmail.com>
2018-05-18 17:34:23 -0400

    :sparkles: allow specifying the branch to install

--- install.sh
+++ install.sh
@@ -1,16 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
-mkdir -p $HOME/.local/share
-export PEARL_ROOT=$HOME/.local/share/pearl
-export PEARL_HOME=$HOME/.config/pearl
+[[ -n "$1" ]] && branch="--branch $1" || unset branch
 
-[ -d "$PEARL_ROOT" ] && { echo "Error: Pearl seems already installed in $PEARL_ROOT. Remove the directory in order to proceed with the installation."; exit 11; }
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+mkdir -p "$XDG_CONFIG_HOME"
+mkdir -p "$XDG_DATA_HOME"
+export PEARL_HOME="$XDG_CONFIG_HOME/pearl"
+export PEARL_ROOT="$XDG_DATA_HOME/pearl"
+
+if [[ -e "$PEARL_ROOT" ]]; then
+	printf '%s' "Error: It seems Pearl is already installed in $PEARL_ROOT."
+	printf '%s\n' ' Remove the directory to proceed with the installation.'
+	exit 11
+fi
 
 echo "* Downloading Pearl to $PEARL_ROOT"
-git clone --quiet 'https://github.com/pearl-core/pearl.git' $PEARL_ROOT
-cd $PEARL_ROOT
-git submodule update --quiet --init --recursive
+git clone --quiet $branch --depth 1  --recurse-submodules \
+	'https://github.com/pearl-core/pearl.git' "$PEARL_ROOT"
 
-$PEARL_ROOT/bin/pearl init
+"$PEARL_ROOT/bin/pearl" init
